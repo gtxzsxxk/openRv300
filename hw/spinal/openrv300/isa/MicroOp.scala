95a8ea76893fa2afeabc004eba79b4d735a8cdef
Hanyuan Zhao <hanyuan-z@qq.com>
2024-09-13 01:14:38 +0800

    isa: micro-op: distinguish binary and shift operations

--- hw/spinal/openrv300/isa/MicroOp.scala
+++ hw/spinal/openrv300/isa/MicroOp.scala
@@ -10,7 +10,9 @@ object MicroOp {
   val BRANCH = U"7'd5"
   val LOAD = U"7'd6"
   val STORE = U"7'd7"
-  val ARITH_IMM = U"7'd8"
-  val ARITH = U"7'd9"
-  val ENV = U"7'd10"
+  val ARITH_BINARY_IMM = U"7'd8"
+  val ARITH_SHIFT_IMM = U"7'd9"
+  val ARITH_BINARY = U"7'd10"
+  val ARITH_SHIFT = U"7'd11"
+  val ENV = U"7'd12"
 }
