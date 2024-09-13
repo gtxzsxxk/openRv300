705b1a8f187a2a5f3f10c284e7e206f2f52491b8
Hanyuan Zhao <hanyuan-z@qq.com>
2024-09-14 00:50:05 +0800

    pipeline: feat: implement the basic register write back

---
+++ hw/spinal/openrv300/pipeline/WriteBackGPRs.scala
@@ -0,0 +1,25 @@
+package openrv300.pipeline
+
+import openrv300.GPRsWritePort
+import openrv300.pipeline.payload.ExecMemPayload
+import spinal.core._
+import spinal.lib._
+
+case class WriteBackGPRs() extends Component {
+  val io = new Bundle {
+    val request = slave(Flow(ExecMemPayload()))
+    val regWritePort = master(GPRsWritePort())
+  }
+
+  val reqData = io.request.payload
+
+  io.regWritePort.writeEnable := False
+  io.regWritePort.writeAddr := U"5'd0"
+  io.regWritePort.writeData := B"32'd0"
+
+  when(io.request.valid && reqData.writeRegDest) {
+    io.regWritePort.writeEnable := True
+    io.regWritePort.writeAddr := reqData.regDest
+    io.regWritePort.writeData := reqData.regDestValue
+  }
+}
