d753ffd845997a3aa2beb07621a5b6ca4dee57ec
gtxzsxxk <2524395907@qq.com>
2024-09-20 12:37:33 +0800

    DDRSim: feat: finish the state machine of ddr sim's read/write

--- hw/spinal/openrv300/utils/DDRSim.scala
+++ hw/spinal/openrv300/utils/DDRSim.scala
@@ -20,20 +20,89 @@ class DDRSim extends Component {
   val simMemory = Mem(Bits(32 bits), wordCount = 0x02000000)
 
   val fsm = new StateMachine {
+    val axiId = Reg(UInt(4 bits))
     val addr = Reg(UInt(32 bits))
-    val len = Reg(Bits(8 bits))
+    val addrByWord = Cat(addr(31 downto 2), U"2'd0").asUInt
+    val len = Reg(UInt(8 bits))
     val size = Reg(Bits(3 bits))
     val burst = Reg(Bits(2 bits))
-    val ready = Reg(Bool())
+    val awReady = Reg(Bool())
+    val arReady = Reg(Bool())
+
+    val counter = Reg(UInt(8 bits))
 
     val init = new State with EntryPoint
-    val goWrite = new State
-    val goRead = new State
+    val doWrite = new State
+    val writeResponse = new State
+    val doRead = new State
 
-    init.whenIsActive {
+    init.onEntry(awReady := False).onEntry(arReady := False).whenIsActive {
       when(aw.valid) {
-        addr := aw.addr
+        val compatible = aw.payload.burst === Axi4.burst.INCR && aw.payload.size === Axi4.size.BYTE_4
 
+        axiId := aw.payload.id
+        addr := aw.payload.addr
+        len := aw.payload.len
+        size := aw.payload.size
+        burst := aw.payload.burst
+        awReady := compatible
+        aw.ready := awReady
+
+        when(compatible) {
+          goto(doWrite)
+        }
+      } elsewhen (ar.valid) {
+        val compatible = ar.payload.burst === Axi4.burst.INCR && ar.payload.size === Axi4.size.BYTE_4
+        axiId := ar.payload.id
+        addr := ar.payload.addr
+        len := ar.payload.len
+        size := ar.payload.size
+        burst := ar.payload.burst
+        arReady := compatible
+        ar.ready := arReady
+
+        when(compatible) {
+          goto(doRead)
+        }
+      }
+    }
+
+    doWrite.onEntry(counter := 0).whenIsActive {
+      aw.setIdle()
+      when(w.valid) {
+        w.ready := True
+        simMemory.write(addrByWord + counter, w.payload.data)
+        counter := counter + 4
+        when(w.last) {
+          goto(writeResponse)
+        }
+      }
+    }
+
+    writeResponse.whenIsActive {
+      w.setIdle()
+      b.payload.resp := Axi4.resp.OKAY
+      b.valid := True
+      b.payload.id := axiId
+      when(b.ready) {
+        b.setIdle()
+        goto(init)
+      }
+    }
+
+    doRead.onEntry(counter := 0).whenIsActive {
+      ar.setIdle()
+      when(r.ready) {
+        when(counter <= len) {
+          r.payload.resp := Axi4.resp.OKAY
+          r.payload.data := simMemory(addrByWord + counter)
+          r.valid := True
+          r.last := counter === len
+          counter := counter + 4
+        } otherwise {
+          r.setIdle()
+          goto(init)
+        }
       }
     }
   }
