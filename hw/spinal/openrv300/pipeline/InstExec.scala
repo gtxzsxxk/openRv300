aedbd63072c6f290d6496ac2d91acad575441d58
gtxzsxxk <2524395907@qq.com>
2024-09-30 16:25:56 +0800

    exec: refactor: use fsm to support executing an instruction which needs more than one cycle

--- hw/spinal/openrv300/pipeline/InstExec.scala
+++ hw/spinal/openrv300/pipeline/InstExec.scala
@@ -3,8 +3,10 @@ package openrv300.pipeline
 import openrv300.isa.MicroOp
 import spinal.core._
 import spinal.lib._
+import spinal.lib.fsm._
 import payload.{DecodePayload, ExecMemPayload, RegisterSourceBundle}
 import control._
+import openrv300.pipeline.muldiv.MulDivExec
 
 case class InstExec() extends Component {
   val io = new Bundle {
@@ -13,6 +15,8 @@ case class InstExec() extends Component {
     val bypassWritePort = master(BypassWritePort())
     val execRegisters = in port Vec.fill(2)(RegisterSourceBundle())
     val isStalling = in port Bool()
+
+    val execNeedStall = out port Bool()
   }
 
   val reqData = io.request.payload
@@ -55,6 +59,11 @@ case class InstExec() extends Component {
     io.execRegisters(idx).value
   }
 
+  val mulDivExec = MulDivExec(3)
+  mulDivExec.io.request.payload := reqData
+  mulDivExec.io.request.valid := False
+  mulDivExec.io.execRegisters := io.execRegisters
+
   def NOP(fakeNop: Boolean = false, microOp: Bits = MicroOp.ARITH_BINARY_IMM): Unit = {
     ansPayload.microOp := microOp
     ansPayload.writeRegDest := True
@@ -68,192 +77,225 @@ case class InstExec() extends Component {
   }
 
   io.answer.payload := ansPayload
-  ansValid := True
+  ansValid := False
 
-  when(io.isStalling || !io.request.valid || ansPayload.takeJump) {
-    /* 译出NOP */
-    NOP()
-  } otherwise {
-    switch(reqData.microOp) {
-      is(MicroOp.LUI) {
-        ansPayload.writeRegDest := True
-        ansPayload.regDestValue := Cat(reqData.imm, B"12'd0")
-        insertBypass(true)
-      }
-      is(MicroOp.AUIPC) {
-        ansPayload.writeRegDest := True
-        ansPayload.regDestValue := (reqData.instPc + Cat(reqData.imm, B"12'd0").asUInt).asBits
-        insertBypass(true)
-      }
-      is(MicroOp.JAL) {
-        ansPayload.takeJump := True
-        ansPayload.jumpPc := reqData.instPc + reqData.sextImm.asUInt
+  io.execNeedStall := False
 
-        ansPayload.writeRegDest := True
-        ansPayload.regDestValue := (reqData.instPc + U"32'd4").asBits
-      }
-      is(MicroOp.JALR) {
-        ansPayload.takeJump := True
-        ansPayload.jumpPc := registerSourceValues(0).asUInt + reqData.sextImm.asUInt
+  val fsm = new StateMachine {
+    val normal = new State with EntryPoint
+    val waitForMulDivExec = new State
 
-        ansPayload.writeRegDest := True
-        ansPayload.regDestValue := (reqData.instPc + U"32'd4").asBits
-      }
-      is(MicroOp.BRANCH) {
-        switch(reqData.function0) {
-          is(B"000") {
-            /* BEQ */
-            ansPayload.takeJump := registerSourceValues(0) === registerSourceValues(1)
+    normal.whenIsActive {
+      when(io.isStalling || !io.request.valid || ansPayload.takeJump) {
+        /* 译出NOP */
+        NOP()
+      } otherwise {
+        ansValid := True
+        switch(reqData.microOp) {
+          is(MicroOp.LUI) {
+            ansPayload.writeRegDest := True
+            ansPayload.regDestValue := Cat(reqData.imm, B"12'd0")
+            insertBypass(true)
           }
-          is(B"001") {
-            /* BNE */
-            ansPayload.takeJump := registerSourceValues(0) =/= registerSourceValues(1)
+          is(MicroOp.AUIPC) {
+            ansPayload.writeRegDest := True
+            ansPayload.regDestValue := (reqData.instPc + Cat(reqData.imm, B"12'd0").asUInt).asBits
+            insertBypass(true)
           }
-          is(B"100") {
-            /* BLT */
-            ansPayload.takeJump := registerSourceValues(0).asSInt < registerSourceValues(1).asSInt
-          }
-          is(B"101") {
-            /* BGE */
-            ansPayload.takeJump := registerSourceValues(0).asSInt >= registerSourceValues(1).asSInt
-          }
-          is(B"110") {
-            /* BLTU */
-            ansPayload.takeJump := registerSourceValues(0).asUInt < registerSourceValues(1).asUInt
-          }
-          is(B"111") {
-            /* BGEU */
-            ansPayload.takeJump := registerSourceValues(0).asUInt >= registerSourceValues(1).asUInt
-          }
-        }
-        default {
-          /* TODO: illegal Inst. */
-        }
+          is(MicroOp.JAL) {
+            ansPayload.takeJump := True
+            ansPayload.jumpPc := reqData.instPc + reqData.sextImm.asUInt
 
-        ansPayload.jumpPc := reqData.instPc + reqData.sextImm.asUInt
+            ansPayload.writeRegDest := True
+            ansPayload.regDestValue := (reqData.instPc + U"32'd4").asBits
+          }
+          is(MicroOp.JALR) {
+            ansPayload.takeJump := True
+            ansPayload.jumpPc := registerSourceValues(0).asUInt + reqData.sextImm.asUInt
 
-        NOP(fakeNop = true)
-      }
-      is(MicroOp.LOAD, MicroOp.STORE) {
-        ansPayload.writeRegDest := reqData.microOp === MicroOp.LOAD
-        ansPayload.memoryAddress := registerSourceValues(0).asUInt + reqData.sextImm.asUInt
+            ansPayload.writeRegDest := True
+            ansPayload.regDestValue := (reqData.instPc + U"32'd4").asBits
+          }
+          is(MicroOp.BRANCH) {
+            switch(reqData.function0) {
+              is(B"000") {
+                /* BEQ */
+                ansPayload.takeJump := registerSourceValues(0) === registerSourceValues(1)
+              }
+              is(B"001") {
+                /* BNE */
+                ansPayload.takeJump := registerSourceValues(0) =/= registerSourceValues(1)
+              }
+              is(B"100") {
+                /* BLT */
+                ansPayload.takeJump := registerSourceValues(0).asSInt < registerSourceValues(1).asSInt
+              }
+              is(B"101") {
+                /* BGE */
+                ansPayload.takeJump := registerSourceValues(0).asSInt >= registerSourceValues(1).asSInt
+              }
+              is(B"110") {
+                /* BLTU */
+                ansPayload.takeJump := registerSourceValues(0).asUInt < registerSourceValues(1).asUInt
+              }
+              is(B"111") {
+                /* BGEU */
+                ansPayload.takeJump := registerSourceValues(0).asUInt >= registerSourceValues(1).asUInt
+              }
+            }
+            default {
+              /* TODO: illegal Inst. */
+            }
 
-        insertBypass(false)
-      }
-      is(MicroOp.ARITH_BINARY_IMM) {
-        ansPayload.writeRegDest := True
-        switch(reqData.function0) {
-          is(B"000") {
-            /* ADDI */
-            ansPayload.regDestValue := (registerSourceValues(0).asUInt + reqData.sextImm.asUInt).asBits
+            ansPayload.jumpPc := reqData.instPc + reqData.sextImm.asUInt
+
+            NOP(fakeNop = true)
           }
-          is(B"010") {
-            /* SLTI */
-            ansPayload.regDestValue := Cat(B"31'd0",
-              registerSourceValues(0).asSInt < reqData.sextImm)
+          is(MicroOp.LOAD, MicroOp.STORE) {
+            ansPayload.writeRegDest := reqData.microOp === MicroOp.LOAD
+            ansPayload.memoryAddress := registerSourceValues(0).asUInt + reqData.sextImm.asUInt
+
+            insertBypass(false)
           }
-          is(B"011") {
-            /* SLTIU */
-            ansPayload.regDestValue := Cat(B"31'd0",
-              registerSourceValues(0).asUInt < reqData.sextImm.asUInt)
+          is(MicroOp.ARITH_BINARY_IMM) {
+            ansPayload.writeRegDest := True
+            switch(reqData.function0) {
+              is(B"000") {
+                /* ADDI */
+                ansPayload.regDestValue := (registerSourceValues(0).asUInt + reqData.sextImm.asUInt).asBits
+              }
+              is(B"010") {
+                /* SLTI */
+                ansPayload.regDestValue := Cat(B"31'd0",
+                  registerSourceValues(0).asSInt < reqData.sextImm)
+              }
+              is(B"011") {
+                /* SLTIU */
+                ansPayload.regDestValue := Cat(B"31'd0",
+                  registerSourceValues(0).asUInt < reqData.sextImm.asUInt)
+              }
+              is(B"100") {
+                /* XORI */
+                ansPayload.regDestValue := (registerSourceValues(0).asUInt ^ reqData.sextImm.asUInt).asBits
+              }
+              is(B"110") {
+                /* ORI */
+                ansPayload.regDestValue := (registerSourceValues(0).asUInt | reqData.sextImm.asUInt).asBits
+              }
+              is(B"111") {
+                /* ANDI */
+                ansPayload.regDestValue := (registerSourceValues(0).asUInt & reqData.sextImm.asUInt).asBits
+              }
+              default {
+                /* TODO: illegal Inst. */
+              }
+            }
+            insertBypass(true)
           }
-          is(B"100") {
-            /* XORI */
-            ansPayload.regDestValue := (registerSourceValues(0).asUInt ^ reqData.sextImm.asUInt).asBits
+          is(MicroOp.ARITH_SLL_IMM) {
+            ansPayload.writeRegDest := True
+            ansPayload.regDestValue := (registerSourceValues(0).asUInt |<< reqData.imm.resize(5).asUInt).asBits
+            insertBypass(true)
           }
-          is(B"110") {
-            /* ORI */
-            ansPayload.regDestValue := (registerSourceValues(0).asUInt | reqData.sextImm.asUInt).asBits
+          is(MicroOp.ARITH_SRL_IMM) {
+            ansPayload.writeRegDest := True
+            ansPayload.regDestValue := (registerSourceValues(0).asUInt |>> reqData.imm.resize(5).asUInt).asBits
+            insertBypass(true)
           }
-          is(B"111") {
-            /* ANDI */
-            ansPayload.regDestValue := (registerSourceValues(0).asUInt & reqData.sextImm.asUInt).asBits
+          is(MicroOp.ARITH_SRA_IMM) {
+            ansPayload.writeRegDest := True
+            ansPayload.regDestValue := (registerSourceValues(0).asSInt >> reqData.imm.resize(5).asUInt).asBits
+            insertBypass(true)
+          }
+          is(MicroOp.ARITH_BINARY) {
+            ansPayload.writeRegDest := True
+            switch(reqData.function1) {
+              is(B"0000000") {
+                switch(reqData.function0) {
+                  is(B"000") {
+                    /* ADD */
+                    ansPayload.regDestValue := (registerSourceValues(0).asUInt + registerSourceValues(1).asUInt).asBits
+                  }
+                  is(B"010") {
+                    /* SLT */
+                    ansPayload.regDestValue := Cat(B"31'd0",
+                      registerSourceValues(0).asSInt < registerSourceValues(1).asSInt).asBits
+                  }
+                  is(B"011") {
+                    /* SLTU */
+                    ansPayload.regDestValue := Cat(B"31'd0",
+                      registerSourceValues(0).asUInt < registerSourceValues(1).asUInt).asBits
+                  }
+                  is(B"100") {
+                    /* XOR */
+                    ansPayload.regDestValue := (registerSourceValues(0).asUInt ^ registerSourceValues(1).asUInt).asBits
+                  }
+                  is(B"110") {
+                    /* OR */
+                    ansPayload.regDestValue := (registerSourceValues(0).asUInt | registerSourceValues(1).asUInt).asBits
+                  }
+                  is(B"111") {
+                    /* AND */
+                    ansPayload.regDestValue := (registerSourceValues(0).asUInt & registerSourceValues(1).asUInt).asBits
+                  }
+                  default {
+                    /* TODO: illegal Inst. */
+                  }
+                }
+              }
+              is(B"0100000") {
+                when(reqData.function0 === B"000") {
+                  /* SUB */
+                  ansPayload.regDestValue := (registerSourceValues(0).asUInt - registerSourceValues(1).asUInt).asBits
+                } otherwise {
+                  /* TODO: illegal Inst. */
+                }
+              }
+              is(B"0000001") {
+                /* 乘法 */
+                ansValid := False
+                mulDivExec.io.request.valid := True
+                goto(waitForMulDivExec)
+              }
+              default {
+                /* TODO: illegal Inst. */
+              }
+            }
+            insertBypass(true)
+          }
+          is(MicroOp.ARITH_SLL) {
+            ansPayload.writeRegDest := True
+            ansPayload.regDestValue := (registerSourceValues(0).asUInt |<< registerSourceValues(1).asUInt.resize(5)).asBits
+            insertBypass(true)
+          }
+          is(MicroOp.ARITH_SRL) {
+            ansPayload.writeRegDest := True
+            ansPayload.regDestValue := (registerSourceValues(0).asUInt |>> registerSourceValues(1).asUInt.resize(5)).asBits
+            insertBypass(true)
+          }
+          is(MicroOp.ARITH_SRA) {
+            ansPayload.writeRegDest := True
+            ansPayload.regDestValue := (registerSourceValues(0).asSInt >> registerSourceValues(1).asUInt.resize(5)).asBits
+            insertBypass(true)
+          }
+          is(MicroOp.ECALL) {
+            NOP(fakeNop = true, microOp = MicroOp.ECALL)
           }
           default {
             /* TODO: illegal Inst. */
           }
         }
-        insertBypass(true)
       }
-      is(MicroOp.ARITH_SLL_IMM) {
-        ansPayload.writeRegDest := True
-        ansPayload.regDestValue := (registerSourceValues(0).asUInt |<< reqData.imm.resize(5).asUInt).asBits
-        insertBypass(true)
-      }
-      is(MicroOp.ARITH_SRL_IMM) {
-        ansPayload.writeRegDest := True
-        ansPayload.regDestValue := (registerSourceValues(0).asUInt |>> reqData.imm.resize(5).asUInt).asBits
-        insertBypass(true)
-      }
-      is(MicroOp.ARITH_SRA_IMM) {
-        ansPayload.writeRegDest := True
-        ansPayload.regDestValue := (registerSourceValues(0).asSInt >> reqData.imm.resize(5).asUInt).asBits
-        insertBypass(true)
-      }
-      is(MicroOp.ARITH_BINARY) {
-        ansPayload.writeRegDest := True
-        when(reqData.function1(0) === False) {
-          switch(reqData.function0) {
-            is(B"000") {
-              /* ADD */
-              ansPayload.regDestValue := (registerSourceValues(0).asUInt + registerSourceValues(1).asUInt).asBits
-            }
-            is(B"010") {
-              /* SLT */
-              ansPayload.regDestValue := Cat(B"31'd0",
-                registerSourceValues(0).asSInt < registerSourceValues(1).asSInt).asBits
-            }
-            is(B"011") {
-              /* SLTU */
-              ansPayload.regDestValue := Cat(B"31'd0",
-                registerSourceValues(0).asUInt < registerSourceValues(1).asUInt).asBits
-            }
-            is(B"100") {
-              /* XOR */
-              ansPayload.regDestValue := (registerSourceValues(0).asUInt ^ registerSourceValues(1).asUInt).asBits
-            }
-            is(B"110") {
-              /* OR */
-              ansPayload.regDestValue := (registerSourceValues(0).asUInt | registerSourceValues(1).asUInt).asBits
-            }
-            is(B"111") {
-              /* AND */
-              ansPayload.regDestValue := (registerSourceValues(0).asUInt & registerSourceValues(1).asUInt).asBits
-            }
-            default {
-              /* TODO: illegal Inst. */
-            }
-          }
-        } otherwise {
-          when(reqData.function0 === B"000") {
-            /* SUB */
-            ansPayload.regDestValue := (registerSourceValues(0).asUInt - registerSourceValues(1).asUInt).asBits
-          } otherwise {
-            /* TODO: illegal Inst. */
-          }
-        }
-        insertBypass(true)
-      }
-      is(MicroOp.ARITH_SLL) {
-        ansPayload.writeRegDest := True
-        ansPayload.regDestValue := (registerSourceValues(0).asUInt |<< registerSourceValues(1).asUInt.resize(5)).asBits
-        insertBypass(true)
-      }
-      is(MicroOp.ARITH_SRL) {
-        ansPayload.writeRegDest := True
-        ansPayload.regDestValue := (registerSourceValues(0).asUInt |>> registerSourceValues(1).asUInt.resize(5)).asBits
-        insertBypass(true)
-      }
-      is(MicroOp.ARITH_SRA) {
-        ansPayload.writeRegDest := True
-        ansPayload.regDestValue := (registerSourceValues(0).asSInt >> registerSourceValues(1).asUInt.resize(5)).asBits
-        insertBypass(true)
-      }
-      is(MicroOp.ECALL) {
-        NOP(fakeNop = true, microOp = MicroOp.ECALL)
-      }
-      default {
-        /* TODO: illegal Inst. */
+    }
+
+    waitForMulDivExec.whenIsActive {
+      ansValid := False
+      io.execNeedStall := True
+      when(mulDivExec.io.answer.valid) {
+        ansValid := True
+        io.execNeedStall := False
+        ansPayload := mulDivExec.io.answer.payload
+        goto(normal)
       }
     }
   }
