9ca1b4b91588460f83d4abe67845a0752e1d3a10
gtxzsxxk <2524395907@qq.com>
2024-09-30 18:54:30 +0800

    muldiv: fix: refactor the blackbox verilog code for simulation to a three stage pipeline

--- hw/spinal/openrv300/pipeline/muldiv/Multiplier.scala
+++ hw/spinal/openrv300/pipeline/muldiv/Multiplier.scala
@@ -5,10 +5,14 @@ import spinal.lib._
 
 case class Multiplier() extends BlackBox {
   val io = new Bundle {
+    val clk = in port Bool()
+    val resetn = in port Bool()
     val A = in port UInt(32 bits)
     val B = in port UInt(32 bits)
     val P = out port UInt(64 bits)
   }
+
+  mapCurrentClockDomain(io.clk, io.resetn, resetActiveLevel = LOW)
   noIoPrefix()
   addRTLPath("hw/spinal/openrv300/pipeline/muldiv/sim/Multiplier.v")
 }
--- hw/spinal/openrv300/pipeline/muldiv/sim/Multiplier.v
+++ hw/spinal/openrv300/pipeline/muldiv/sim/Multiplier.v
@@ -1,9 +1,25 @@
 module Multiplier (
+    input clk,
+    input resetn,
     input [31:0] A,
     input [31:0] B,
     output [63:0] P
 );
 
-assign P = A * B;
+reg [31:0] A_1;
+reg [31:0] B_1;
+reg [31:0] A_2;
+reg [31:0] B_2;
+reg [63:0] Po;
+
+assign P = Po;
+
+always @ (posedge clk) begin
+    A_1 <= A;
+    B_1 <= B;
+    A_2 <= A_1;
+    B_2 <= B_1;
+    Po <= A_2 * B_2;
+end
 
 endmodule
