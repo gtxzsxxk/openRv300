827e7d7e44367857d2dc7e331ef26731c53f5b57
gtxzsxxk <2524395907@qq.com>
2024-10-01 22:01:16 +0800

    pipeline: feat: introduce a fetch buffer that shared by fetch and decode

--- hw/spinal/openrv300/OpenRv300.scala
+++ hw/spinal/openrv300/OpenRv300.scala
@@ -31,6 +31,7 @@ case class OpenRv300() extends Component {
 
   val gprs = GPRs()
   val bypassUnit = BypassUnit()
+  val fetchBuffer = FetchBuffer(2)
 
   val iCache = Cache(2)
   val dCache = Cache(2)
