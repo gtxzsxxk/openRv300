03d9a34cf462f601fad63954d611526b59f83a31
gtxzsxxk <2524395907@qq.com>
2024-10-06 01:09:09 +0800

    privilege: feat: add bundle when csr unit requests fetch to do a trap

---
+++ hw/spinal/openrv300/privilege/DoTrapInformation.scala
@@ -0,0 +1,15 @@
+package openrv300.privilege
+
+import spinal.core._
+import spinal.lib._
+
+case class DoTrapInformation() extends Bundle with IMasterSlave {
+  val trapJumpAddress = UInt(32 bits)
+  val trapValid = Bool()
+  val trapReady = Bool()
+
+  override def asMaster(): Unit = {
+    out(trapJumpAddress, trapValid)
+    in(trapReady)
+  }
+}
