a52e7b71be4d396237c2fb1811cd8105bca52149
Hanyuan Zhao <hanyuan-z@qq.com>
2024-10-08 21:54:26 +0800

    tests: feat: add mtvec setting and a new section for trap handler

--- hw/spinal/openrv300/tests/bareMetalCPrograms/badaddr.c
+++ hw/spinal/openrv300/tests/bareMetalCPrograms/badaddr.c
@@ -1,5 +1,27 @@
+void exception_handler() __attribute__((section(".trap_handler"), aligned(32)));
+
+void exception_handler() {
+    int sum = 0;
+    for(int i = 1; i <= 5; i++) {
+        sum += i;
+    }
+    asm volatile (
+        "csrrw x29, 0x341, x0\n"   /* mepc */
+        "addi  x29, x29, 4\n"
+        "csrrw x0, 0x341, x29\n"
+        "mret\n"
+    );
+}
+
 int main() {
-    volatile int *a = 0x30000000;
+    unsigned long trap_handler = 0x80003000;
+
+    asm volatile (
+        "csrrw  x0, 0x305, %0\n"  /* mtvec */
+        :"=r"(trap_handler)
+    );
+
+    volatile int *a = (volatile int*)0x30000000;
     *a = 3;
     return 0;
 }
